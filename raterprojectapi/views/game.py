cc923aee98bc99c665c23e5fb7a570a151c29cce
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-14 10:02:05 -0500

    build: game viewset and serializer

--- raterprojectapi/views/game.py
+++ raterprojectapi/views/game.py
@@ -1,4 +1,6 @@
 """View module for handling requests about games"""
+from raterprojectapi.models.game_categories import GameCategory
+from raterprojectapi.models.categories import Category
 from django.core.exceptions import NON_FIELD_ERRORS, ValidationError
 from rest_framework import status
 from django.http import HttpResponseServerError
@@ -30,3 +32,65 @@ class GameView(ViewSet):
 
         except ValidationError as ex:
             return Response({"reason: ex.message"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(Self, request, pk):
+
+        try:
+            game = Game.objects.get(pk=pk)
+            serializer = GameSerializer(game, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def destroy(self, request, pk):
+
+        try:
+            game = Game.objects.get(pk=pk)
+            game.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Game.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def list(self, request):
+
+        games = Game.objects.all()
+
+        gamecategory = self.request.query_params.get('category', None)
+        if gamecategory is not None:
+            games = games.filter(category__id=gamecategory)
+
+        serializer = GameSerializer(
+            games, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+
+        game = Game()
+        game.title = request.data["title"]
+        game.description = request.data["description"]
+        game.designer = request.data["designer"]
+        game.release = request.data["release"]
+        game.number_of_players = request.data["numberOfPlayers"]
+        game.duration = request.data["duration"]
+        game.age = request.data["age"]
+        gameplayer = Player.objects.get(pk=request.data["playerId"])
+        game.player = gameplayer
+        gamecategory = GameCategory.objects.get(pk=request.data['gameCategoryId'])
+        game.category = gamecategory
+        game.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'description', 'designer', 'release', 'numberOfPlayers', 'duration', 'age', 'playerId', 'gameCategoryId')
+        depth = 1
+
+
